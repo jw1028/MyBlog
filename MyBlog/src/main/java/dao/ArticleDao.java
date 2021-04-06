66e4988481cdd036f8b0265eff8fa36079a92daa
jw1028 <55475696+jw1028@users.noreply.github.com>
2021-04-06 21:43:31 +0800

    Update ArticleDao.java

--- MyBlog/src/main/java/dao/ArticleDao.java
+++ MyBlog/src/main/java/dao/ArticleDao.java
@@ -12,6 +12,9 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class ArticleDao {
+    /**
+     * 个人文章列表
+     */
     public List<Article> getListByUID(int uid) throws SQLException {
         List<Article> list = new ArrayList<Article>();
         Connection connection = DBUtil.getConnection();
@@ -29,6 +32,79 @@ public class ArticleDao {
             article.setCreateTime(resultSet.getDate("create_time"));
             list.add(article);
         }
+        DBUtil.close(connection, statement, resultSet);
         return list;
     }
+
+    /**
+     * 删除文章
+     */
+
+    public int delArticle(int id) throws SQLException {
+        int result = 0;
+        Connection connection = DBUtil.getConnection();
+        String sql = "delete from article where id = ?";
+        PreparedStatement statement = connection.prepareStatement(sql);
+        statement.setInt(1,id);
+        // 真正的执行操作
+        result = statement.executeUpdate();
+        // 关闭数据库连接
+        DBUtil.close(connection, statement, null);
+        return result;
+    }
+
+    /**
+     * 添加文章
+     */
+    public int addArticle(String title, String content, int uid) throws SQLException {
+        int result = 0;
+
+        Connection connection = DBUtil.getConnection();
+        String sql = "insert into article(title,content,user_id) values(?,?,?)";
+        PreparedStatement statement = connection.prepareStatement(sql);
+        statement.setString(1, title);
+        statement.setString(2, content);
+        statement.setInt(3, uid);
+
+        // 执行sql
+        result = statement.executeUpdate();
+        DBUtil.close(connection, statement, null);
+        return result;
+    }
+
+    /**
+     * 修改文章
+     */
+    public int modifiyArticle(String title, String content, int id) throws SQLException {
+        int result = 0;
+        Connection connection = DBUtil.getConnection();
+        String sql = "update article set title=?, content=? where id=?";
+        PreparedStatement statement = connection.prepareStatement(sql);
+        statement.setString(1, title);
+        statement.setString(2, content);
+        statement.setInt(3, id);
+        result = statement.executeUpdate();
+        DBUtil.close(connection, statement, null);
+        return result;
+    }
+
+    /**
+     * 查询文章
+     * @param id
+     * @return
+     */
+    public Article queryArticle(int id) throws SQLException {
+        Article article = new Article();
+        Connection connection = DBUtil.getConnection();
+        String sql = "select * from article where id=?";
+        PreparedStatement statement = connection.prepareStatement(sql);
+        statement.setInt(1, id);
+        ResultSet resultSet = statement.executeQuery();
+        if (resultSet.next()) {
+            article.setTitle(resultSet.getString("title"));
+            article.setContent(resultSet.getString("content"));
+        }
+        return article;
+    }
 }
+
