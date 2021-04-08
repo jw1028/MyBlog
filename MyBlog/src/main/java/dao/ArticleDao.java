43e3c8d76ba4041e33ec7784cea39656b57da184
jw1028 <55475696+jw1028@users.noreply.github.com>
2021-04-08 22:48:46 +0800

    Update ArticleDao.java

--- MyBlog/src/main/java/dao/ArticleDao.java
+++ MyBlog/src/main/java/dao/ArticleDao.java
@@ -2,6 +2,7 @@ package dao;
 
 
 import model.Article;
+import model.vo.ArticleVO;
 import util.DBUtil;
 
 import java.sql.Connection;
@@ -15,7 +16,7 @@ public class ArticleDao {
     /**
      * 个人文章列表
      */
-    public List<Article> getListByUID(int uid) throws SQLException {
+    public List<Article> getListByID(int uid) throws SQLException {
         List<Article> list = new ArrayList<Article>();
         Connection connection = DBUtil.getConnection();
         String sql = "select * from article where user_id=?";
@@ -26,10 +27,10 @@ public class ArticleDao {
         while (resultSet.next()) {
             Article article = new Article();
             article.setId(resultSet.getInt("id"));
-            article.setReadCount(resultSet.getInt("rcount"));
+            article.setReadCount(resultSet.getInt("readCount"));
             article.setTitle(resultSet.getString("title"));
             article.setContent(resultSet.getString("content"));
-            article.setCreateTime(resultSet.getDate("create_time"));
+            article.setCreateTime(resultSet.getDate("createTime"));
             list.add(article);
         }
         DBUtil.close(connection, statement, resultSet);
@@ -89,22 +90,84 @@ public class ArticleDao {
     }
 
     /**
-     * 查询文章
-     * @param id
-     * @return
+     * 通过id查询文章查询文章
      */
-    public Article queryArticle(int id) throws SQLException {
-        Article article = new Article();
+    public Article getArticleById(int id) throws SQLException {
+        ArticleVO article = new ArticleVO();
         Connection connection = DBUtil.getConnection();
-        String sql = "select * from article where id=?";
+        String sql = "select a.*,u.username from article a left join user u on a.user_id=u.id where a.id=?";
         PreparedStatement statement = connection.prepareStatement(sql);
         statement.setInt(1, id);
         ResultSet resultSet = statement.executeQuery();
         if (resultSet.next()) {
             article.setTitle(resultSet.getString("title"));
             article.setContent(resultSet.getString("content"));
+            article.setCreateTime(resultSet.getDate("create_time"));
+            article.setReadCount(resultSet.getInt("rcount"));
+            article.setUsername(resultSet.getString("username"));
         }
+        DBUtil.close(connection, statement, resultSet);
         return article;
+    }
+    /**
+     * 获取所有人的文章列表
+     */
+    public List<ArticleVO> getAllList() throws SQLException {
+        List<ArticleVO> list = new ArrayList<ArticleVO>();
+        Connection connection = DBUtil.getConnection();
+        String sql = "select a.*,u.username from article a left join user u on a.user_id=u.id";
+        PreparedStatement statement = connection.prepareStatement(sql);
+        ResultSet resultSet = statement.executeQuery();
+        while (resultSet.next()) {
+            ArticleVO vo = new ArticleVO();
+            vo.setId(resultSet.getInt("id"));
+            vo.setUsername(resultSet.getString("username"));
+            vo.setTitle(resultSet.getString("title"));
+            vo.setReadCount(resultSet.getInt("rcount"));
+            vo.setCreateTime(resultSet.getDate("create_time"));
+            list.add(vo);
+        }
+        DBUtil.close(connection, statement, resultSet);
+        return list;
+    }
+
+    /**
+     * 根据分页查询文章列表
+     */
+
+    public List<ArticleVO> getListByPage(int cpage, int psize) throws SQLException {
+        List<ArticleVO> list = new ArrayList<ArticleVO>();
+        Connection connection = DBUtil.getConnection();
+        String sql = "select a.*,u.username from article a left join user u on a.user_id=u.id limit ?,?";
+        PreparedStatement statement = connection.prepareStatement(sql);
+        statement.setInt(1, (cpage - 1) * psize);
+        statement.setInt(2, psize);
+        ResultSet resultSet = statement.executeQuery();
+        while (resultSet.next()) {
+            ArticleVO vo = new ArticleVO();
+            vo.setId(resultSet.getInt("id"));
+            vo.setUsername(resultSet.getString("username"));
+            vo.setTitle(resultSet.getString("title"));
+            vo.setReadCount(resultSet.getInt("rcount"));
+            vo.setCreateTime(resultSet.getDate("create_time"));
+            list.add(vo);
+        }
+        DBUtil.close(connection, statement, resultSet);
+        return list;
+    }
+
+    /**
+     * 阅读量增加
+     */
+    public int readCountAdd (int id) throws SQLException {
+        int result = 0;
+        Connection connection = DBUtil.getConnection();
+        String sql = "update article set rcount=rcount+1 where id=?";
+        PreparedStatement statement = connection.prepareStatement(sql);
+        statement.setInt(1, id);
+        result = statement.executeUpdate();
+        DBUtil.close(connection, statement, null);
+        return result;
     }
 }
 
